6da2f4717a8a12a871fda254ebac6b5edcd47593
Linpyj <atarimaetaisou@icloud.com>
2021-09-27 21:48:35 +0900

    fix

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,16 +1,48 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, Image, Dimensions, Modal, TextInput } from 'react-native';
-import {FloatingActionButton} from '../components/FloatingActionButton';
+import { FloatingActionButton} from '../components/FloatingActionButton';
+// import { CreateReviewScreen } from '../components/CreateReviewScreen';
 
 export const DetailScreen = ({navigation, route}) => {
-  console.log(route.params);
-  // const { shop } = route.params;
-  // console.log(shop)
+  const { shop } = route.params;
+  // console.log(sho@)
   const [modalVisible, setModalVisible] = useState(false);
 
   return (
     <View>
-      <Text>I</Text>
+      <Image
+        source={{ uri: shop.imageUrl }}
+        style={{ width: "100%", height: "50%" }}
+      />
+      <Text
+        style={{ fontWeight: "bold", fontSize: 30, marginTop: 10, marginLeft: 10 }}
+      >
+        {shop.name}
+      </Text>
+      <Text
+        style={{ fontSize: 15, marginTop: 10, marginLeft: 10 }}
+      >
+        {shop.place}
+      </Text>
+      <Button
+        title="open modal"
+        onPress={() => {
+          setModalVisible(true);
+        }}
+      />
+      <FloatingActionButton
+        iconName="plus"
+        onPress = {() => {
+          navigation.navigate("CreateReview")
+        }}
+      />
+      
+      <Button
+        title="Home画面に遷移する"
+        onPress={() => {
+          navigation.navigate('CreateReview');
+        }}
+      />
     </View>
   );
 }
